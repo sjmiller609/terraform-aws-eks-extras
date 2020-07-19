374576055524417089d2a9a5a8ba7b4d675e265e
Steven Miller <sjmiller609@gmail.com>
2020-07-19 18:43:15 +0000

    Add Prometheus server

--- system_components.tf
+++ system_components.tf
@@ -20,4 +20,10 @@ resource "helm_release" "vertical-pod-autoscaler" {
   version    = "2.0.0"
 }
 
-
+resource "helm_release" "prometheus" {
+  chart      = "prometheus"
+  name       = "prometheus"
+  namespace  = kubernetes_namespace.system-components.metadata.0.name
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  version    = "11.7.0"
+}
